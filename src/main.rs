8586eb87cae56a34eb04854be9f948b9cad0b2f2
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-06 21:57:11 +0100

    creating more routes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 use axum::{
     routing::get,
+    extract::Path,
     Router,
 };
+
+async fn root()->String{
+    let s = String::from("root");
+    return s
+}
+async fn user()->String{
+    let s = String::from("All users");
+    return s
+}
+async fn user_id(Path(id):Path<u32>)->String{
+    format!("User ID: {}", id)
+}
 #[tokio::main]
 async fn main() {
-    let app =  Router::new().route("/",get(|| async { "Hello, World!" }));
+    let app =  Router::new()
+        .route("/",get(root))
+        .route("/user",get(user))
+        .route("/user/{id}",get(user_id));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+
