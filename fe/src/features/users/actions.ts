623f316eb5dbb851bbbcbda1b1cd4814e2a31fa2
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-11 16:49:28 +0100

    function to post new user

--- fe/src/features/users/actions.ts
+++ fe/src/features/users/actions.ts
@@ -1,11 +1,17 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 type Users = {
   id: string;
   name: string;
   email: string;
 };
 
+type NewUser = {
+  name: string;
+  email: string;
+};
 export async function getAllUsers() {
   const response = await fetch("http://localhost:3000/users");
   if (!response.ok) {
@@ -14,4 +20,19 @@ export async function getAllUsers() {
   const data = await response.json();
 
   return data.users;
+}
+
+export async function addUser(data: NewUser) {
+  try {
+    const response = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  } catch (error) {
+    console.log("Error:", error);
+  }
+  revalidatePath("/users");
 }
