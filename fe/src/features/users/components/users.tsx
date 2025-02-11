ed24bae6f0282267263d101b0b6baa2ebaeafc07
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-11 16:49:04 +0100

    removed old code

--- fe/src/features/users/components/users.tsx
+++ fe/src/features/users/components/users.tsx
@@ -4,11 +4,9 @@ import { User } from "./user";
 
 export default async function Users() {
   const users: UserType[] = await getAllUsers();
-  console.log("users", users);
 
   return (
     <div>
-      <p className="m-2">users</p>
       {users.map((user, index) => (
         <User user={user} key={index} />
       ))}
